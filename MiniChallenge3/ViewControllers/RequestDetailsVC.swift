0faf47735c66442aba5fc08040da54b709c680b5
alhanouf20 <alhanouf228@gmail.com>
2022-03-01 11:25:13 +0300

    commit alhanouf

--- MiniChallenge3/ViewControllers/RequestDetailsVC.swift
+++ MiniChallenge3/ViewControllers/RequestDetailsVC.swift
@@ -32,6 +32,16 @@ class RequestDetailsVC: UIViewController {
         
     }
     
+    @IBAction func confirmreq(_ sender: Any) {
+        let alert = UIAlertController(title: "Pending", message: "Your request is pending now, you will be notified once the organizer accept it", preferredStyle:.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true) {
+            self.performSegue(withIdentifier: "notificationScreen", sender: self)
+        }
+    }
+    
+    
     @IBAction func stepBtn(_ sender: UIStepper) {
         labeln.text = String(Int(sender.value))
         print(sender.value)
