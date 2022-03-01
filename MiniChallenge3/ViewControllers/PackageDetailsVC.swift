97fb996a44348dfb0d78c6d81b12b698121afbc6
FatimahAlmatter <f-y.1416@hotmail.com>
2022-03-01 11:15:57 +0300

    fatimah

--- MiniChallenge3/ViewControllers/PackageDetailsVC.swift
+++ MiniChallenge3/ViewControllers/PackageDetailsVC.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class PackageDetailsVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class PackageDetailsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+  @IBOutlet weak var PckgDetails: UICollectionView!
+  var arrpackgeCatalog = [UIImage(named: "pkg1"), UIImage(named: "pkg2"), UIImage(named: "pkg3")]
+    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    PckgDetails.delegate = self
+    PckgDetails.dataSource = self
+  }
+    
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return arrpackgeCatalog.count
+  }
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellP", for: indexPath) as! PackageDetailsCollectionViewCell
+    cell.imgCatalog.image = arrpackgeCatalog[indexPath.row]
+    cell.layer.cornerRadius = 20
+    return cell
+  }
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 128, height: collectionView.frame.height)
+  }
 }
